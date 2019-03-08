843fd3f4672c0719ca65ef749213a27a1bd99696
Tudor <timedemo@gmail.com>
2019-03-08 17:24:24 +0200

    Added Access-Control-Allowed-Methods

--- server2.js
+++ server2.js
@@ -15,9 +15,9 @@ app.use(function (req, res, next) {
     "use strict";
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
     next();
 });
-
 
 // WEB SERVICE ENDPOINTS (REST API services)
 
